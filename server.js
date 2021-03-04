e8776b0033714c436cda18897eaae27df8a75b51
mnawale <mnawale@uncc.edu>
2021-03-04 16:37:48 -0500

    aa

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const options = {
             version: '2.1.1',
             description: 'Student information '
         },
-        host:'localhost:3000',
+        host:'itis-6177-swagger-nnv8w.ondigitalocean.app/',
         basePath: '/',
     },
     apis: ['./server.js'],
